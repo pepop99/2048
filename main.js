f2100e6a3aaa3f78b7101f9d4132ff97ce38a455
Rishabh Arora <rishabharora1780@gmail.com>
2017-12-30 00:51:00 +0530

    added left and right movement

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 child = document.getElementsByClassName("child");
 
 var i;
+var s1 = Math.floor(Math.random() * 16);
+var sn1 = Math.floor(Math.random() * 1) + 2;
+var s2 = Math.floor(Math.random() * 16);
+var sn2 = Math.floor(Math.random() * 3) * 2;
+var num = [];
+var num1 = [];
+
 var color = function(){
 	for(i=0;i<16;i++)
 	{		
@@ -32,8 +39,178 @@ var color = function(){
 			child[i].style.background="#ecc440";
 		else if(child[i].innerHTML==2048)
 			child[i].style.background="#ecc12f";
+		else if(child[i].innerHTML==0)
+			{
+				child[i].style.color="#ccc0b3";
+				child[i].style.background="#ccc0b3";
+			}
 	}
 };
 color();
+
+var begin = function(){
+	child[s1].innerHTML=sn1;
+	if(s1==s2)
+		s2=15-s1;
+	if(sn2==0)
+		sn2=2;
+	child[s2].innerHTML=sn2;
+	color();
+};
+
+// var j=0;
+// var begin = function(){
+// 	num[j]=s1;
+// 	num1[j]=sn1;
+// 	console.log(num[j],num1[j]);
+// 	j++;
+// 	num[j]=s1;
+// 	num1[j]=sn1;
+// 	console.log(num[j],num1[j]);
+// 		color();
+// };
+
+begin();
+
+var left = function(){
+	console.log("left");
+	var i,j;
+	for(j=0;j<2;j++)
+	{
+	for(i=3;i>0;i--)
+	{
+		if(child[i].innerHTML==child[i-1].innerHTML){
+			child[i-1].innerHTML=child[i-1].innerHTML*2;
+			child[i].innerHTML=0;
+		}
+		else if(child[i].innerHTML-child[i-1].innerHTML==child[i].innerHTML){
+			console.log("yes");
+			child[i-1].innerHTML=child[i].innerHTML;
+			child[i].innerHTML=0;
+		}
+	}
+	for(i=7;i>4;i--)
+	{
+		if(child[i].innerHTML==child[i-1].innerHTML){
+			child[i-1].innerHTML=child[i-1].innerHTML*2;
+			child[i].innerHTML=0;
+		}
+		else if(child[i].innerHTML-child[i-1].innerHTML==child[i].innerHTML){
+			console.log("yes");
+			child[i-1].innerHTML=child[i].innerHTML;
+			child[i].innerHTML=0;
+		}
+	}
+	for(i=11;i>8;i--)
+	{
+		if(child[i].innerHTML==child[i-1].innerHTML){
+			child[i-1].innerHTML=child[i-1].innerHTML*2;
+			child[i].innerHTML=0;
+		}
+		else if(child[i].innerHTML-child[i-1].innerHTML==child[i].innerHTML){
+			console.log("yes");
+			child[i-1].innerHTML=child[i].innerHTML;
+			child[i].innerHTML=0;
+		}
+	}
+	for(i=15;i>12;i--)
+	{
+		if(child[i].innerHTML==child[i-1].innerHTML){
+			child[i-1].innerHTML=child[i-1].innerHTML*2;
+			child[i].innerHTML=0;
+		}
+		else if(child[i].innerHTML-child[i-1].innerHTML==child[i].innerHTML){
+			console.log("yes");
+			child[i-1].innerHTML=child[i].innerHTML;
+			child[i].innerHTML=0;
+		}
+	}
+	color();
+}
+	
+}	
+
+var up = function(){
+	console.log("up")
+}
+
+var right = function(){
+	console.log("right");
+	var i,j;
+	for(j=0;j<2;j++)
+	{
+	for(i=0;i<3;i++)
+	{
+		if(child[i].innerHTML==child[i+1].innerHTML){
+			child[i+1].innerHTML=child[i+1].innerHTML*2;
+			child[i].innerHTML=0;
+		}
+		else if(child[i].innerHTML-child[i+1].innerHTML==child[i].innerHTML){
+			console.log("yes");
+			child[i+1].innerHTML=child[i].innerHTML;
+			child[i].innerHTML=0;
+		}
+	}
+	for(i=4;i<7;i++)
+	{
+		if(child[i].innerHTML==child[i+1].innerHTML){
+			child[i+1].innerHTML=child[i+1].innerHTML*2;
+			child[i].innerHTML=0;
+		}
+		else if(child[i].innerHTML-child[i+1].innerHTML==child[i].innerHTML){
+			console.log("yes");
+			child[i+1].innerHTML=child[i].innerHTML;
+			child[i].innerHTML=0;
+		}
+	}
+	for(i=8;i<11;i++)
+	{
+		if(child[i].innerHTML==child[i+1].innerHTML){
+			child[i+1].innerHTML=child[i+1].innerHTML*2;
+			child[i].innerHTML=0;
+		}
+		else if(child[i].innerHTML-child[i+1].innerHTML==child[i].innerHTML){
+			console.log("yes");
+			child[i+1].innerHTML=child[i].innerHTML;
+			child[i].innerHTML=0;
+		}
+	}
+	for(i=12;i<15;i++)
+	{
+		if(child[i].innerHTML==child[i+1].innerHTML){
+			child[i+1].innerHTML=child[i+1].innerHTML*2;
+			child[i].innerHTML=0;
+		}
+		else if(child[i].innerHTML-child[i+1].innerHTML==child[i].innerHTML){
+			console.log("yes");
+			child[i+1].innerHTML=child[i].innerHTML;
+			child[i].innerHTML=0;
+		}
+	}
+	color();
+}
+
+}
+var down = function(){
+	console.log("down");
+}
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            left();
+            break;
+        case 38:
+            up();
+            break;
+        case 39:
+            right();
+            break;
+        case 40:
+            down();
+            break;
+    }
+};
+
 
 
